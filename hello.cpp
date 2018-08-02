3d197ddc2b6828412b554d66a9da52486670ef12
Bianca <binaksrc@gmail.com>
2018-08-01 21:01:41 -0300

    Initial commit

---
+++ hello.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+/**
+* \brief Calcula base^exp
+* \param base Base da potencia
+*	  	 exp  Expoente da potencia
+* \return base^exp
+*/
+
+unsigned long long toPow( int base, int exp );
+
+int main () {
+// Pegar um numero n entre 0 e 64
+	std::cout << "Digite um numero entre 0 e 64" << std::endl;
+	int n = 0;
+	std::cin >> n
+// Verificar se ele esta no range correto
+	if (n < 0 || n > 64) {
+		std::cout << "numero invalido";
+		return;
+	}
+// Calcular e mostrar 2 elevado a [0;n]
+	for (auto i (0); i <= 64; ++i)
+		std::cout << "2 elevado a " << i << "eh igual a " <<  toPow(2,n) << std::endl;
+
+	return 0;
+}
+
+unsigned long long toPow( int base, int exp ) {
+	
+}

5a4da32104eea86da1689e5359088547b4ff8d4c
Bianca Rodrigues <binaksrc@gmail.com>
2018-08-04 20:50:44 -0300

    fix more things

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
-/**
+/*!
 * \brief Calcula base^exp
 * \param base Base da potencia
-*	  	 exp  Expoente da potencia
+* \param exp  Expoente da potencia
 * \return base^exp
 */
 
@@ -13,15 +13,15 @@ int main () {
 // Pegar um numero n entre 0 e 64
 	std::cout << "Digite um numero entre 0 e 64" << std::endl;
 	int n = 0;
-	std::cin >> n
+	std::cin >> n;
 // Verificar se ele esta no range correto
 	if (n < 0 || n > 64) {
 		std::cout << "numero invalido";
-		return;
+		return -1;
 	}
 // Calcular e mostrar 2 elevado a [0;n]
-	for (auto i (0); i < 64; ++i)
-		std::cout << "2 elevado a " << i << "eh igual a " <<  toPow(2,n) << std::endl;
+	for (auto i (0); i <= n; ++i)
+		std::cout << "2 elevado a " << i << " eh igual a " <<  toPow(2,i) << std::endl;
 
 	return 0;
 }
@@ -30,5 +30,5 @@ unsigned long long toPow( int base, int exp ) {
 // Base da exponenciacao
 	if (exp == 0) return 1;
 // Recursao
-	return (base * toPow (base, --exp));
+	return base * (toPow (base, (exp - 1)));
 }

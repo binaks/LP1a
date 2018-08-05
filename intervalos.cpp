5633957b6dbb4961fad0adb074defa22e4e62097
Bianca <binaksrc@gmail.com>
2018-08-04 21:40:35 -0300

    add intervalos.cpp

---
+++ intervalos.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+main () {
+	int x;
+
+	std::cout << "Entre com valores inteiros (Ctrl+d p/ encerrar): " << std::endl;
+
+// Ler cada valor enquanto o usuario nao pressionar <Ctrl+d>
+	while (std::cin >> x) {
+		
+	}
+
+// Iterar quantos valores foram lidos
+
+// Verficar quantos deles estao em cada um dos intervalos:
+
+// [0,25)
+
+// [25,50)
+
+// [50,75)
+
+// [75,100]	
+
+// Calcular a porcentagem de valores dentro de cada um dos intervalos
+
+//Exibir contagem para os intervalos solicitados
+
+
+	return 0;
+}

30d0e067251c19389e0a16b4981224981ce2367d
Bianca Rodrigues <binaksrc@gmail.com>
2018-08-05 18:30:29 -0300

    add troca_seguintes_vet.cpp

---
+++ troca_seguintes_vet.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+// Imprimir B
+void printB (int b[20]);
+
+int main () {
+
+    int n;
+    int b[20];
+    int tmp;
+
+// Ler 20 inteiros
+    for (auto i(0); i < 20; i++) {
+        std::cout << "Entre com um inteiro (posicao " << i << "): " << std::endl;
+        std::cin >> n;
+// Armazenar os inteiros em um vetor B
+        b[i] = n;
+    }
+
+    std::cout << std::endl;
+
+    printB(b);
+
+// Trocar o ultimo elemento de B com o 1o, o penultimo pelo 2o, ...
+    for (auto i(0); i < 20; i++) {
+        if ((i%2) == 1) {
+            tmp = b[i];
+            b[i] = b[i+1];
+            b[i+1] = tmp;
+        }
+    }
+
+// Imprimir o vetor modificado
+    printB(b);
+
+    return 0;
+}
+
+void printB (int b[20]) {
+    for (auto i(0); i < 20; i++) {
+        if (i == 0)
+            std::cout << "B = [";
+        if (i < 19)
+            std::cout << b[i] << ",";
+        if (i == 19)
+            std::cout << b[i] << "]" << std::endl;
+    }
+}
